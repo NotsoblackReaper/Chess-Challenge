f86df815edf05fb66200f3f9364167f8a0ae611c
Jonas Demski <jonasd1305@gmail.com>
2023-07-22 00:34:41 +0200

    first tests

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,31 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 public class MyBot : IChessBot
 {
+    //Phases 
+    //0=Opening
+    //2=Middle Game
+    //3=Endgame
+    int phase = 0;
+
+    Dictionary<ulong, Move> storedMoves = new Dictionary<ulong, Move>();
+
     public Move Think(Board board, Timer timer)
     {
+        ulong key = board.ZobristKey;
+        if (storedMoves.ContainsKey(key)) return storedMoves[key];
+        Move move = new Move();
+
+
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        move = moves[new Random().Next(moves.Length)];
+
+        storedMoves[key] = move;
+        return move; ;
     }
 }
